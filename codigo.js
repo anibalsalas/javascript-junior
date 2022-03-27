3831e6b14ddc0804dcc187ea6c758f5b2a3647d2
Anival Salas <66155820+anibalsalas@users.noreply.github.com>
2022-03-27 16:09:34 -0500

    prg1

--- codigo.js
+++ codigo.js
@@ -83,18 +83,116 @@
 // document.write(frase)
 
 //OPERADORES LOGICOS
-let n1= 23;
-let n2 = 13;
-let t1 = "23"
-let t2 = "t2"
+// let n1= 23;
+// let n2 = 13;
+// let t1 = "23"
+// let t2 = "t2"
 //let resultado = n1> n3;
 //document.write(n1 == n2)
 //document.write(n1 != n2)
 //document.write(n1 === t1) //se compara si son estrictamente iguales en este caso omite si es tipo numero o texto
 //document.write(resultado)
 
-a1= n1 > n2;
-a2= n1 == n2;
-document.write(a1 && a2)
-//muestra True
-document.write(a1 || a2)
+// a1= n1 > n2;
+// a2= n1 == n2;
+// document.write(a1 && a2)
+// //muestra True
+// document.write(a1 || a2)
+// //muestra false
+
+//solo se ejecutarà el bloque si el if es verdadero
+// if(10>15){
+//     alert("hola");
+//     document.write("pedro")
+// }
+//////////////////////
+// nombre = "Salas"
+// if (nombre == "Anival"){
+//     alert("tu nombre es " + nombre)
+// }
+//     else if  (nombre == "Salas"){
+//         alert("tu nombre es " + nombre)
+//     } 
+///////////////////////
+// nombre = "Salas"
+// if (nombre == "Anival"){
+//     alert("tu nombre es Correcto")
+// }
+//     else if  (nombre == "Perez"){
+//         alert("tu apellido es Perez")
+//     }
+//     else{
+//         alert("tu Primer Apellido es Salas")
+//     }
+
+// nombre = "Anival"
+// apellido = "Salas"
+// if (nombre == "Anival" && apellido == "Salas"){
+//     alert("tu nombre es Correcto")
+//  }
+dineroCofla = prompt("Cuanto dinero tienes Cofla?");
+dineroRoberto = prompt("Cuanto dinero tienes Roberto?");
+dineroPedro = prompt("Cuanto dinero tienes Pedro?");
+
+if (dineroCofla > 0.6 && dineroCofla <1){
+    alert("Cofla comprate el helado de agua")
+}
+else if (dineroCofla >= 1 && dineroCofla < 1.6){
+    alert("Cofla comprate el helado de crema")
+}
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7){
+    alert("Cofla comprate el helado de heladix")
+}
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8){
+    alert("Cofla comprate el helado de heladovich")
+}
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9){
+    alert("Cofla comprate el helado de helardo")
+}
+else if (dineroCofla >= 2.9){
+    alert("Cofla helado con confites o el pote de 1/4kg");
+} else {
+    alert(" Coflalo siento, no te alcanza para ningun helado")
+}
+///////////////
+if (dineroRoberto > 0.6 && dineroRoberto <1){
+    alert("Roberto comprate el helado de agua")
+}
+else if (dineroRoberto >= 1 && dineroRoberto < 1.6){
+    alert("Roberto comprate el helado de crema")
+}
+else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7){
+    alert("Roberto comprate el helado de heladix")
+}
+else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8){
+    alert("Roberto comprate el helado de heladovich")
+}
+else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9){
+    alert("Roberto comprate el helado de helardo")
+}
+else if (dineroRoberto >= 2.9){
+    alert("Roberto helado con confites o el pote de 1/4kg");
+} else {
+    alert(" Roberto lo siento, no te alcanza para ningun helado")
+}
+//////////////////
+if (dineroPedro > 0.6 && dineroPedro <1){
+    alert("Pedro cómprate el helado de agua")
+}
+else if (dineroPedro >= 1 && dineroPedro < 1.6){
+    alert("Pedro cómprate el helado de crema")
+}
+else if (dineroPedro >= 1.6 && dineroPedro < 1.7){
+    alert("Pedro cómprate el helado de heladix")
+}
+else if (dineroPedro >= 1.7 && dineroPedro < 1.8){
+    alert("Pedro cómprate el helado de heladovich")
+}
+else if (dineroPedro >= 1.8 && dineroPedro < 2.9){
+    alert("Pedro cómprate el helado de helardo")
+}
+else if (dineroPedro >= 2.9){
+    alert("Pedro helado con confites o el pote de 1/4kg");
+} else {
+    alert(" Pedro lo siento, no te alcanza para ningún helado")
+}
