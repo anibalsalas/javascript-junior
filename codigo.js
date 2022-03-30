bcfc4be79b008757990792f55d52966679428a35
Anival Salas <66155820+anibalsalas@users.noreply.github.com>
2022-03-30 01:41:29 -0500

    bubles

--- codigo.js
+++ codigo.js
@@ -130,69 +130,168 @@
 // if (nombre == "Anival" && apellido == "Salas"){
 //     alert("tu nombre es Correcto")
 //  }
-dineroCofla = prompt("Cuanto dinero tienes Cofla?");
-dineroRoberto = prompt("Cuanto dinero tienes Roberto?");
-dineroPedro = prompt("Cuanto dinero tienes Pedro?");
+//programa 1
+// dineroCofla = prompt("Cuanto dinero tienes Cofla?");
+// dineroRoberto = prompt("Cuanto dinero tienes Roberto?");
+// dineroPedro = prompt("Cuanto dinero tienes Pedro?");
 
-if (dineroCofla > 0.6 && dineroCofla <1){
-    alert("Cofla comprate el helado de agua")
+// dineroCofla = parse(dineroCofla);//convierte entero a numero
+
+// if (dineroCofla > 0.6 && dineroCofla <1){
+//     alert("Cofla comprate el helado de agua");
+//     alert("y te sobran" + dineroCofla - 0.6);
+// }
+// else if (dineroCofla >= 1 && dineroCofla < 1.6){
+//     alert("Cofla comprate el helado de crema");
+//     alert("y te sobran" + (dineroCofla - 1));
+// }
+// else if (dineroCofla >= 1.6 && dineroCofla < 1.7){
+//     alert("Cofla comprate el helado de heladix");
+//     alert("y te sobran" + (dineroCofla - 1.6));
+// }
+// else if (dineroCofla >= 1.7 && dineroCofla < 1.8){
+//     alert("Cofla comprate el helado de heladovich");
+//     alert("y te sobran" + (dineroCofla - 1.7));
+// }
+// else if (dineroCofla >= 1.8 && dineroCofla < 2.9){
+//     alert("Cofla comprate el helado de helardo");
+//     alert("y te sobran" + (dineroCofla - 1.8));
+// }
+// else if (dineroCofla >= 2.9){
+//     alert("Cofla helado con confites o el pote de 1/4kg");
+// } else {
+//     alert(" Coflalo siento, no te alcanza para ningun helado")
+// }
+// ///////////////
+// if (dineroRoberto > 0.6 && dineroRoberto <1){
+//     alert("Roberto comprate el helado de agua")
+// }
+// else if (dineroRoberto >= 1 && dineroRoberto < 1.6){
+//     alert("Roberto comprate el helado de crema")
+// }
+// else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7){
+//     alert("Roberto comprate el helado de heladix")
+// }
+// else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8){
+//     alert("Roberto comprate el helado de heladovich")
+// }
+// else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9){
+//     alert("Roberto comprate el helado de helardo")
+// }
+// else if (dineroRoberto >= 2.9){
+//     alert("Roberto helado con confites o el pote de 1/4kg");
+// } else {
+//     alert(" Roberto lo siento, no te alcanza para ningun helado")
+// }
+// //////////////////
+// if (dineroPedro > 0.6 && dineroPedro <1){
+//     alert("Pedro cómprate el helado de agua")
+// }
+// else if (dineroPedro >= 1 && dineroPedro < 1.6){
+//     alert("Pedro cómprate el helado de crema")
+// }
+// else if (dineroPedro >= 1.6 && dineroPedro < 1.7){
+//     alert("Pedro cómprate el helado de heladix")
+// }
+// else if (dineroPedro >= 1.7 && dineroPedro < 1.8){
+//     alert("Pedro cómprate el helado de heladovich")
+// }
+// else if (dineroPedro >= 1.8 && dineroPedro < 2.9){
+//     alert("Pedro cómprate el helado de helardo")
+// }
+// else if (dineroPedro >= 2.9){
+//     alert("Pedro helado con confites o el pote de 1/4kg");
+// } else {
+//     alert(" Pedro lo siento, no te alcanza para ningún helado")
+// }
+// PROGRAMA 1 -SIMPLIFICADO
+// "use strict";
+// const definirCompra = (n)=>{
+//     let din = prompt(`Dinero de ${n}`);
+//     if din >= 0.6 && din < 1) return (`${n}: Helado de agua`);
+//     if din >= 1 && din < 1.6) return (`${n}: Helado de crema`);
+//     if din >= 1.6 && din < 1.7) return (`${n}: Helado de heladix`);
+//     if din >= 1.7 && din < 1.8) return (`${n}: Helado de heladovich`);
+//     if din >= 1.8 && din < 2.9) return (`${n}: Helado de Helardo`);
+//     if din >= 2.9) return (`${n}: Helado de fresa, chocolate de 1/4kg`);
+//     else return (`${n}: No te alcanza para ningun helado`);
+// }
+
+// console.log(definirCompra("Cofla"));
+// console.log(definirCompra("Pedro"));
+// console.log(definirCompra("Roberto"));
+
+//ARRAY
+// let array= ["banana","manzana","pera"];
+// /////////0,1,2///posiciones del array
+// //document.write(array)
+// //mostrar primer elemento
+// //document.write(array[0]); ///trae el elemento de esa posición
+
+// //ARRAY ASOCIATIVOS
+// let pc = {
+//     nombre : "AnivalPC",
+//     procesador : "Intel Core I7",
+//     ram : "8gb",
+//     disco : "1tb"
+// }
+// let nombre = pc["nombre"];
+// let procesador = pc["procesador"];
+// let ram = pc["ram"];
+// let espacio = pc["disco"];
+// frase =`El nombre de mi PC es :<b> ${nombre}</b><br>
+// EL Procesador es : ${procesador}<br>
+// La memoria ram es : ${ram}`
+
+// document.write(frase);
+
+//BUCLES E ITERACIÓN
+// let n1 = 0
+
+// if (n1 < 10){
+//     n1++;//el ++ le aumenta 1
+//     n1++;
+//     document.write(n1)
+// }
+// //muestra: 2 
+// let n = 0
+// while (n < 6){
+//     n++;//el ++ le aumenta 1
+//     document.write(n)
+// }
+// //muestra: 123456
+// let nx = 0
+// while (nx < 6){
+//     nx++;//el ++ le aumenta 1
+//     document.write(nx + "<br>")
+// }
+// //muestra: 
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+// // 6
+// // 7
+
+// while ( numero < 1000){
+//     numero++;
+//     document.write(numero);
+//     if (numero == 10){
+//         break;//finaliza
+//     }
+// }
+// document.write("fin")
+// //muestra: 12345678910fin
+
+for (let i = 0; i < 6; i++){//declaracion e inicializaciòn;condicion; aumento o decremento
+    document.write(i + "<br>")
 }
-else if (dineroCofla >= 1 && dineroCofla < 1.6){
-    alert("Cofla comprate el helado de crema")
-}
-else if (dineroCofla >= 1.6 && dineroCofla < 1.7){
-    alert("Cofla comprate el helado de heladix")
-}
-else if (dineroCofla >= 1.7 && dineroCofla < 1.8){
-    alert("Cofla comprate el helado de heladovich")
-}
-else if (dineroCofla >= 1.8 && dineroCofla < 2.9){
-    alert("Cofla comprate el helado de helardo")
-}
-else if (dineroCofla >= 2.9){
-    alert("Cofla helado con confites o el pote de 1/4kg");
-} else {
-    alert(" Coflalo siento, no te alcanza para ningun helado")
-}
-///////////////
-if (dineroRoberto > 0.6 && dineroRoberto <1){
-    alert("Roberto comprate el helado de agua")
-}
-else if (dineroRoberto >= 1 && dineroRoberto < 1.6){
-    alert("Roberto comprate el helado de crema")
-}
-else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7){
-    alert("Roberto comprate el helado de heladix")
-}
-else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8){
-    alert("Roberto comprate el helado de heladovich")
-}
-else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9){
-    alert("Roberto comprate el helado de helardo")
-}
-else if (dineroRoberto >= 2.9){
-    alert("Roberto helado con confites o el pote de 1/4kg");
-} else {
-    alert(" Roberto lo siento, no te alcanza para ningun helado")
-}
-//////////////////
-if (dineroPedro > 0.6 && dineroPedro <1){
-    alert("Pedro cómprate el helado de agua")
-}
-else if (dineroPedro >= 1 && dineroPedro < 1.6){
-    alert("Pedro cómprate el helado de crema")
-}
-else if (dineroPedro >= 1.6 && dineroPedro < 1.7){
-    alert("Pedro cómprate el helado de heladix")
-}
-else if (dineroPedro >= 1.7 && dineroPedro < 1.8){
-    alert("Pedro cómprate el helado de heladovich")
-}
-else if (dineroPedro >= 1.8 && dineroPedro < 2.9){
-    alert("Pedro cómprate el helado de helardo")
-}
-else if (dineroPedro >= 2.9){
-    alert("Pedro helado con confites o el pote de 1/4kg");
-} else {
-    alert(" Pedro lo siento, no te alcanza para ningún helado")
-}
+//muestra:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+///////////////////////////////////////////
